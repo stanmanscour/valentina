b60c844beb9cc87cb2510eb78d6d29ba918397f8
Stan <stanislas.manscour@hetic.net>
2017-08-22 15:47:44 +0200

    add 'close the player'

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="player">
+    <div v-if="media" class="player">
+        <a @click="closeSong" class="player--closeBtn"><img src="/src/assets/icons/closePlayer.svg"></a>
         <iframe v-if="media === 'youtube'" class="player__youtube" id="ytplayer" type="text/html" width="300" height="300" :src="'https://www.youtube.com/embed/'+song.src + '?rel=0&autoplay=1'" frameborder="0" />
-        <iframe v-if="media === 'soundcloud'" class="player__soundcloud" width="100%" height="450" scrolling="no" frameborder="no" :src="'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+song.src+'&amp;auto_play=false&amp;&amp;color=0066cc'"></iframe>
+        <iframe v-if="media === 'soundcloud'" class="player__soundcloud" width="100%" height="450" scrolling="no" frameborder="no" :src="'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+song.src"></iframe>
     </div>
 </template>
 
@@ -11,13 +12,17 @@
     export default {
         data: function() {
             return {
-                media: null,
+                media: true,
                 song: null
             }
         },
         methods: {
             playSong(){
                 this.media = this.song.media;
+                console.log(this.song);
+            },
+            closeSong(){
+                this.media = 0;
             }
         },
         created(){
@@ -33,8 +38,6 @@
     @import './../assets/scss/mixins.scss';
 
     .player {
-        &__youtube,
-        &__soundcloud {
             opacity: 0;
             visibility: hidden;
             position: fixed;
@@ -54,6 +57,35 @@
                     opacity: 1;
                 }
             }
+        
+        &--closeBtn {
+            position: absolute;
+            right: -15px;
+            top: -15px;
+            transition: all .2s ease;
+
+            img {
+                height: 30px;
+                width: 30px;
+                transition: all .2s ease;
+            }
+
+            &:hover {
+                cursor: pointer;
+                right: -22.5px;
+                top: -22.5px;
+
+                img {
+                    height: 45px;
+                    width: 45px;
+                }
+            }
+        }
+
+        &__soundcloud,
+        &__youtube {
+            width: 100%;
+            height: 100%;
         }
     }
 </style>
