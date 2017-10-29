e996ded5fe4fe66c04bd6462d10eb4f4e49c6834
Stan <stanislas.manscour@hetic.net>
2017-10-29 19:11:11 +0100

    add search addon

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,42 +1,64 @@
 <template>
-  <header class="topHeader">
-    <div class="topHeader__left">
-      <img class="topHeader__left--logoMobile" src="src/assets/icons/logo-valentina.svg" alt="">
-      <a class="topHeader__left--logoDesktop" href="#">valentina.io</a>
-      <a class="topHeader__left--navigationDesktop" href="#">-> explorer</a>
-    </div>
-    <div class="topHeader__middle">
-      <!-- bibliothèque -->
+  <header>
+    <div class="topHeader">
+      <div class="topHeader__left">
+        <img class="topHeader__left--logoMobile" src="src/assets/icons/logo-valentina.svg" alt="">
+        <a class="topHeader__left--logoDesktop" href="#">valentina.io</a>
+        <a class="topHeader__left--navigationDesktop" href="#">-> explorer</a>
+      </div>
+      <div class="topHeader__middle">
+        <!-- bibliothèque -->
 
-      <template>
-        <a class="topHeader__middle--addLink" href="#">Ajouter <img src="src/assets/icons/white-add.svg"></a>
-        <a class="topHeader__middle--searchLink" href="#">Rechercher <img src="src/assets/icons/white-research.svg"></a>
-      </template>
-      
-      <!-- -->
+        <template>
+          <a class="topHeader__middle--addLink" href="#">Ajouter <img src="src/assets/icons/white-add.svg"></a>
+          <a class="topHeader__middle--searchLink" href="#">Rechercher <img src="src/assets/icons/white-research.svg"></a>
+        </template>
+        
+        <!-- -->
+      </div>
+      <div class="topHeader__right">
+        <a class="topHeader__right--accountLink" href="#">Mon compte</a>
+        <a class="topHeader__right--configLink" href="#"><img src="src/assets/icons/white-research.svg"></a>
+      </div>
     </div>
-    <div class="topHeader__right">
-      <a class="topHeader__right--accountLink" href="#">Mon compte</a>
-      <a class="topHeader__right--configLink" href="#"><img src="src/assets/icons/white-research.svg"></a>
+    <div class="addonHeader">
+      <div class="addonHeader__search" v-if="">
+        <input class="addonHeader__search--input" type="text" placeholder="Rechercher dans la bibliothèque">
+      </div>
     </div>
   </header>
 </template>
 
+<script>
+  import { mapGetters } from 'vuex';
+  import { mapActions } from 'vuex';
+
+  export default {
+  }
+</script>
+
 <style lang="scss">
   @import './../assets/scss/mixins.scss';
 
-  .topHeader {
-    box-shadow: 0px 1px 5px rgba(1, 107, 85, 0.59);
+  header {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    box-shadow: 1px 5px 20px 0px rgba(1, 107, 85, 0.16);
     background-color: #00a884;
+  }
+
+  .topHeader {
     padding-left: 30px;
     padding-right: 30px;
     height: 60px;
     width: 100%;
     display: flex;
     flex-direction: row;
-    position: fixed;
-    top: 0px;
-    left: 0px;
+    
+    box-sizing: border-box;
+    
     z-index: 350; // not real
     justify-content: space-between;
 
@@ -163,12 +185,32 @@
         }
       }
     }
-    
+  }
+
+  .addonHeader {
+    &__search {
+      height: 60px;
+      //background-color: blue;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+
+      &--input {
+        width: 90%;
+        font-weight: 500;
+        font-size: 23px;
+        height: 80%;
+        background-color: transparent;
+        border: none;
+        outline: none;
+        color: white;
+        text-align: center;
+
+        &::placeholder {
+          color: rgba(255, 255, 255, 0.45);
+        }
+      }
+    }
   }
 </style>
-
-<script>
-  export default {
-
-  }
-</script>
