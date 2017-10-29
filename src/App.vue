8bad12a43e65ad6b60639d23842675eb1d1d1a1c
Stan <stanislas.manscour@hetic.net>
2017-10-29 14:19:44 +0100

    change scrollbar general

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,22 @@
 
 <style lang="scss">
   @import './assets/scss/reset.scss';
+
+  body {
+    &::-webkit-scrollbar {
+          height: 12px;
+          width: 2px;
+          background: #016752;
+        }
+
+        &::-webkit-scrollbar-thumb {
+            background: #025645;
+            -webkit-border-radius: 1ex;
+            -webkit-box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.75);
+        }
+
+        &::-webkit-scrollbar-corner {
+            background: #000;
+        }
+  }
 </style>
