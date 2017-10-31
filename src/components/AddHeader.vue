81f6fd5a3f6bf746e43b468070802a1a979cbb0b
Stan <stanislas.manscour@hetic.net>
2017-10-31 18:59:33 +0100

    vuex into addHeader file

--- src/components/AddHeader.vue
+++ src/components/AddHeader.vue
@@ -43,6 +43,9 @@
 </template>
 
 <script> 
+  import { mapActions } from 'vuex';
+  import { mapGetters } from 'vuex';
+
   export default {
     data(){
       return {
@@ -58,10 +61,17 @@
         }
       }
     },
-    methods: {
+    computed: {
+      ...mapGetters([
 
+      ])
+    },
+    methods: {
+      ...mapActions([
+        
+      ]),
       verifyUrl(){
-        //
+        // msgError
       },
       goUrl(){
         this.stepUrl = true;
